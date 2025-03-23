f839aa00d9293f862d1a846066d2a351f3a35f5b
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-03-23 15:31:55 -0400

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ThreatDashboard from './components/ThreatDashboard';
+import AlertSystem from './components/AlertSystem';
+
+function App() {
+    return (
+        <div className="App">
+            <ThreatDashboard />
+            <AlertSystem />
+        </div>
+    );
+}
+
+export default App;
