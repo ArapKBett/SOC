44ac22d0e29a8fc240a41cf86db595ca3281230b
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-03-24 17:55:55 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api.threats import get_threat_data
 from api.alerts import get_alerts
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable Cross-Origin Resource Sharing
 
 @app.route('/threats', methods=['GET'])
 def threats():
